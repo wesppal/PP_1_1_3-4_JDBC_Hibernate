850bfe8b6830745231d045f441a338c71bd81a70
Pavel Ivanchik <ivanchik.pal@gmail.com>
2022-07-13 11:08:47 +0300

    C8
    fix and update

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,29 +1,28 @@
 package jm.task.core.jdbc;
 
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-
-import java.util.ArrayList;
-import java.util.List;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
         User user1 = new User("Doc", "Oct", (byte) 34);
         User user2 = new User("Don", "Pomidor", (byte) 15);
         User user3 = new User("Clark", "Kent", (byte) 24);
         User user4 = new User("Black", "Widow", (byte) 30);
-
-        userDao.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-        userDao.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
-        userDao.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
-        userDao.getAllUsers().forEach(System.out::println);
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
-
-
+        userService.saveUser(user1.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        userService.removeUserById(2);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        Util.closeConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Util {
 
@@ -11,14 +10,27 @@ public class Util {
     private static final String connectionString = "jdbc:mysql://127.0.0.1:3306/katapp";
     private static final String login = "root";
     private static final String password = "root";
+    private static Connection connection = null;
 
 
-    public static Connection getConnection() throws  SQLException{
-        return DriverManager.getConnection(connectionString, login, password);
+    public static Connection getConnection() {
+        try {
+            Class.forName(driverName);
+            connection = DriverManager.getConnection(connectionString, login, password);
+            System.out.println("Connection is open.");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 
-    public static String getDriver() {
-        return driverName;
+    public static void closeConnection() {
+            try {
+                connection.close();
+                System.out.println("Connection closed.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
     }
 
 }
